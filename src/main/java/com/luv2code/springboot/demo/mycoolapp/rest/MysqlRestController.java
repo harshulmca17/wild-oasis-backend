8f47cfe7895e423f62204bbba064dcf95d37cba8
harshulk47 <harshulk47@gmail.com>
2024-05-19 18:45:31 +0530

    Update

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/MysqlRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/MysqlRestController.java
@@ -120,6 +120,25 @@ public class MysqlRestController {
         return result;
     }
     @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping("/helloworld")
+    public Result helloworld() throws IOException, ParseException {
+        Result result = new Result();
+        result.setError(null);
+        result.setStatus(HttpStatus.OK.value());
+//        JSONObject mergedJSON = me
+        System.out.println("getBookings");
+        try {
+
+            result.setMessage("heeloworld");
+            result.setResult("heeloworld");
+        } catch (Exception e) {
+            result.setError(e.getMessage());
+            result.setStatus(500);
+
+        }
+        return result;
+    }
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping("/bookings")
     public Result getBookingsByFilter(@RequestBody JSONObject request) throws IOException, ParseException {
         Result result = new Result();
